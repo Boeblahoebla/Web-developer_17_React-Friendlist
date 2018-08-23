e083a96f57c22bf31e64ee251e7ea55ea221ef11
Dzengiz <dzengiz.tafa@telenet.be>
2018-08-23 11:42:05 +0200

    changed the way the app component gets created from a function using const and arrow to a class definition to handle state in the future

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // Imports
 //////////
-import React from 'react';         // View library
+import React, { Component } from 'react';         // View library
 import Cardlist from './Cardlist'; // The full Cardlist
 import {robots} from './robots';   // The robots object for data
 
 // this is the view for the whole app
-const App = () => {
-    return(
-        // return the div holding the full Cardlist 
-        // where every robot has its own Card
-        <Cardlist robots={robots}/>
-    )
+class App extends Component {
+    render() {
+        return(
+            // return the div holding the full Cardlist 
+            // where every robot has its own Card
+            <Cardlist robots={robots}/>
+        )
+    }
 }
 
 // Export the full App to be used on the page
